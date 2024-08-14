24833fc64e0627580cea204b5310dc32482dd10d
Meranoeti <meranojohnmichael8@gmail.com>
2024-08-14 02:53:48 +0100

    index.php

--- index.php
+++ index.php
@@ -9,23 +9,23 @@
 </head>
 
 <body>
-    
-<main>
-    <form action= "includes/formhandler.php" method="post">
-        <label for="firstname">firstname</label>
-        <label id="firstname" type="text" name="firstname" placeholder="firstname>
 
-        <label for="lastname">lastname</label>
-        <label id="lastname" type="text" name="lastname" placeholder="lastname>
+    <main>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">firstname</label>
+            <input id="firstname" type="text" name="firstname" placeholder="firstname">
 
-        <label for="gender">gender ?</label>
-        <select id="gender"name="gender">
-            <option value="none">none</option>
-            <option value="male">male</option>
-            <option value="female">female</option>
-            
-        </select>
-        <button type="submit">submit</button>
-    </form>
+            <label for="lastname">lastname</label>
+            <input id="lastname" type="text" name="lastname" placeholder="lastname">
+
+            <label for="gender">gender ?</label>
+            <select id="gender" name="gender">
+                <option value="none">none</option>
+                <option value="male">male</option>
+                <option value="female">female</option>
+
+</select>
+<button type="submit">submit</button>
+</form>
 </body>
 </html>
