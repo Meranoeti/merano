8249fe517c72fa712b8121f711c80cc5a3f19372
Meranoeti <meranojohnmichael8@gmail.com>
2024-08-14 02:13:03 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<main>
+    <form action= "includes/formhandler.php" method="post">
+        <lable for="firstname">firstname</lable>
+        <lable id="firstname" type="text" name="firstname" placeholder="firstname>
+
+        <lable for="lastname">lastname</lable>
+        <lable id="lastname" type="text" name="lastname" placeholder="lastname>
+
+        <lable for="gender">gender ?>/lable>
+        <select id="gender"name="gender">
+            <option value="none">none</oiption>
+            <option value="male">male</oiption>
+            <option value="female">female</oiption>
+
+</select>
+<button type="submit">submit</button
+</form>
+
+</body>
+</html>
